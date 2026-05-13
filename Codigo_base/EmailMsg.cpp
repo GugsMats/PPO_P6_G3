5ab8358c9644987f1765c071ee18a87ad0dddd97
jpsantos-13 <jpsantos07@ua.pt>
2026-05-13 14:16:21 +0100

    EmailMsg completo (João)

--- Codigo_base/EmailMsg.cpp
+++ Codigo_base/EmailMsg.cpp
@@ -3,3 +3,28 @@
 //
 
 #include "EmailMsg.h"
+
+//inicialização
+unsigned int EmailMsg::nextId = 1;
+
+//defeito
+EmailMsg::EmailMsg(): id(0), srcAddr(""), dstAddr("") {}
+
+//definido
+EmailMsg::EmailMsg(string sa, string da): id(nextId++), srcAddr(sa), dstAddr(da) {}
+
+//getters
+unsigned int EmailMsg::getId() const {return id;} 
+string EmailMsg::getSrcAddr() const {return srcAddr;}
+string EmailMsg::getDstAddr() const {return dstAddr;}
+
+//impressão
+virtual void print() const {
+   cout << "\n"
+        cout << "Id: " << id << "\n";
+        cout << "Origem: " << srcAddr << "\n";
+        cout << "Destino: " << dstAddr << "\n";
+}
+
+#endif //PROJETO_EMAILMSG_H
+#include "EmailMsg.h"
