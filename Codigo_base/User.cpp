6ecabbff8db92d18333942dadbfed1691ba9bab9
jpsantos-13 <jpsantos07@ua.pt>
2026-05-13 14:32:09 +0100

    User.cpp (João)

--- Codigo_base/User.cpp
+++ Codigo_base/User.cpp
@@ -4,5 +4,22 @@
 
 #include "User.h"
 
+//defeito
+User:: User(): nome(""), email(""), numTele("") {}
 
-User :: User(string n, string e, string nt) : nome(n), email(e), numTele(nt) {};
+//definido
+User:: User(string n, string e, string nt): nome(n , email(e), numTele(nt)
+
+//getters
+string User:: getNome() const {return nome;}
+string User:: getEmail() const {return email;}
+string User:: getNumTele() const {return nemTele;}
+
+//impressão
+virtual void print() const {
+  cout << "\n"
+        cout << "Nome: " << nome << "\n";
+        cout << "Email: " << email << "\n";
+        cout << "Número Telemóvel: " << numTele << "\n";
+  };
+}
