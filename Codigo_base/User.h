203565566d3cad16d928f40e8c215c1f5ba0b2d8
jpsantos-13 <jpsantos07@ua.pt>
2026-05-13 13:11:03 +0100

    public User (João)

--- Codigo_base/User.h
+++ Codigo_base/User.h
@@ -18,9 +18,21 @@ private:
     string numTele;
 
 public:
-
-
-    User(string n, string e, string nt);
+    //construtor defeito
+    User(): nome(""), email(""), numTele("") {}
+    //construtor definido
+    User(string n, string e, string nt): nome(n), email(e), numTele(nt) {}
+    //getters
+    string getNome() {return nome;}
+    string getEmail() {return email;}
+    string getNumTele() {return numTele;}
+    //impressão
+    virtual void print() const {
+        cout << "\n";
+        cout << "Nome: " << nome << "\n";
+        cout << "Email: " << email << "\n";
+        cout << "Número de Telemóvel: " << numTele << "\n";
+    }      
 
 };
 
