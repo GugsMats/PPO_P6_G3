139cb93fe4947d73664f748e4a928efd698b6607
jpsantos-13 <jpsantos07@ua.pt>
2026-05-13 13:29:43 +0100

    EmailMsg (João)

--- Codigo_base/EmailMsg.h
+++ Codigo_base/EmailMsg.h
@@ -11,9 +11,24 @@ class EmailMsg {
 
 private:
     unsigned int id;
-
+    string srcAddr;
+    string dstAddr;
 public:
-    
+    //construtor defeito
+    EmailMsg(): id(0), srcAddr(""), dstAddr("") {}
+    //construtor definido
+    EmailMsg(unsigned int iden, string sa, string da): id(iden), srcAddr(sa), dstAddr(da) {}
+    //getters
+    unsigned int getId() {return id;}
+    string getSrcAddr() {return srcAddr;}
+    string getDstAddr() {return dstAddr;}
+    //impressão
+    virtual void print() const {
+        cout << "\n"
+        cout << "Id: " << id << "\n";
+        cout << "Origem: " << srcAddr << "\n";
+        cout << "Destino: " << dstAddr << "\n";
+    }
 
 
 
