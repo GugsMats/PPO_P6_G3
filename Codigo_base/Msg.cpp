cd861eee30df6baed091abeb13533e0c9efb84af
jpsantos-13 <jpsantos07@ua.pt>
2026-05-14 13:56:24 +0100

    Update Msg.cpp

--- Codigo_base/Msg.cpp
+++ Codigo_base/Msg.cpp
@@ -3,3 +3,18 @@
 //
 
 #include "Msg.h"
+
+//defeito
+Msg:: Msg(): id(0), info("") {}
+
+//definido
+Msg:: Msg(string i): info(i) {}
+
+//getters
+Msg:: getInfo() const {return info;}
+
+//impressão
+virtual void print() const {
+  cout << "\n";
+  cout << "Informação: " << info << "\n";
+}
